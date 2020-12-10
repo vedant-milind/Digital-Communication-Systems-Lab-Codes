acd3fe830ea0b7eb7b0c213a3f9b1c9d03122d3f
Vedant Athavale <newmm727699@gmail.com>
2020-12-10 15:08:37 +0530

    Update exp9_10psk.m

--- DCS Lab Experiment 9&10/exp9_10psk.m
+++ DCS Lab Experiment 9&10/exp9_10psk.m
@@ -1,165 +1,77 @@
 clc;
-clear;
+clear all;
 close all;
- 
-M=4;
-nbit=16;                
-msg=round(rand(nbit,1));
-disp('Binary information at transmitter ');
-disp(msg);
-fprintf('\n');
- 
-x=msg;
-bp=.000001;             
-bit=[]; 
-for n=1:1:length(x)
-    if x(n)==1
-       se=ones(1,100);
-    elseif x(n)==0
-        se=zeros(1,100);
-    end
-     bit=[bit se];
-end
- 
-t1=bp/100:bp/100:100*length(x)*(bp/100);
- 
+data=[0 1 0 1 1 1 0 0 1 1]; % information
 figure(1)
+stem(data, 'linewidth',3), grid on;
+title(' Information before Transmiting ');
+axis([ 0 11 0 1.5]);
+data_NZR=2*data-1; % Data Represented at NZR form for QPSK modulation
+s_p_data=reshape(data_NZR,2,length(data)/2); % S/P convertion of data
+br=10.^6; %Let us transmission bit rate 1000000
+f=br; % minimum carrier frequency
+T=1/br; % bit duration
+t=T/99:T/99:T; % Time vector for one bit information
+%QPSK modulation
+y=[];
+y_in=[];
+y_qd=[];
+for(i=1:length(data)/2)
+ y1=s_p_data(1,i)*cos(2*pi*f*t); % inphase component
+ y2=s_p_data(2,i)*sin(2*pi*f*t) ;% Quadrature component
+ y_in=[y_in y1]; % inphase signal vector
+ y_qd=[y_qd y2]; %quadrature signal vector
+ y=[y y1+y2]; % modulated signal vector
+end
+Tx_sig=y; % transmitting signal after modulation
+tt=T/99:T/99:(T*length(data))/2;
+figure(2)
 subplot(3,1,1);
-plot(t1,bit,'lineWidth',2.5);grid on;
-axis([ 0 bp*length(x) -.5 1.5]);
-ylabel('Amplitude(volt)');
-xlabel('Time(sec)');
-title('Transmitting information as digital signal');
- 
-msg_reshape=reshape(msg,log2(M),nbit/log2(M))';
-disp('Information are reshaped for convert symbolic form');
-disp(msg_reshape);
-fprintf('\n');
- 
-size(msg_reshape);
-for j=1:1:nbit/log2(M)
-   for i=1:1:log2(M)
-       a(j,i)=num2str(msg_reshape(j,i));
-   end
-end
- 
-as=bin2dec(a);
-ass=as';
-figure(1)
+plot(tt,y_in,'linewidth',3), grid on;
+title(' wave form for inphase component in QPSK modulation ');
+xlabel('time(sec)');
+ylabel(' amplitude(volt0');
 subplot(3,1,2);
-stem(ass,'Linewidth',2.0);
-title('serial symbol for M-ary modulation at transmitter');
-xlabel('n(discrete time)');
-ylabel('Magnitude');
- 
-disp('Symbolic form information for M-ary');
-disp(ass);
-fprintf('\n');
- 
-x1=[0:M-1];
-p=pskmod(ass,M);
-sym=0:1:M-1;    
-pp=pskmod(sym,M);
-scatterplot(pp),grid on;
-title('Constellation diagram for M-ary');
- 
-RR=real(p);
-II=imag(p);
-sp=bp*2;                                   
-sr=1/sp;                           
-f=sr*2;
-t=sp/100:sp/100:sp;
-ss=length(t);
-m=[];
-for k=1:1:length(RR)
-    yr=RR(k)*cos(2*pi*f*t);        
-    yim=II(k)*sin(2*pi*f*t);       
-    y=yr+yim;
-    m=[m y];
-end
- 
-tt=sp/100:sp/100:sp*length(RR);
- 
-figure(1);
+plot(tt,y_qd,'linewidth',3), grid on;
+title(' wave form for Quadrature component in QPSK modulation ');
+xlabel('time(sec)');
+ylabel(' amplitude(volt0');
 subplot(3,1,3);
-plot(tt,m);
-title('Waveform for M-ary modulation acording to symbolic information');
-xlabel('Time(sec)');
-ylabel('Amplitude(volt)');
- 
-figure(3);
-om=awgn(m,15);
-subplot(3,1,1);
-plot(tt,om);
-title('Waveform for M-ary modulation acording to symbolic information with noise');
-xlabel('Time(sec)');
-ylabel('Amplitude(volt)');
- 
-% M-array QAM demodulation
-m1=[];
-m2=[];
-for n=ss:ss:length(m)
-  t=sp/100:sp/100:sp;
-  y1=cos(2*pi*f*t);  
-  y2=sin(2*pi*f*t);  
-  mm1=y1.*m((n-(ss-1)):n);                                    
-  mm2=y2.*m((n-(ss-1)):n);                                    
-  z1=trapz(t,mm1);   
-  z2=trapz(t,mm2) ;  
-  zz1=round(2*z1/sp);
-  zz2=round(2*z2/sp);
-  m1=[m1 zz1];
-  m2=[m2 zz2];
-end
- 
-clear i;
-clear j;
-for (k=1:1:length(m1))  
-gt(k)=m1(k)+j*m2(k);
-end
- 
-ax=pskdemod(gt,M);
-subplot(3,1,2);
-stem(ax,'linewidth',2);
-title('Re-obtain symbol after M-ary demodulation ');
-xlabel('n(discrete time)');
-ylabel('Magnitude');
- 
-disp('Re-obtain symbol after M-ary demodulation ');
-disp(ax);
-fprintf('\n');
- 
-bi_in=dec2bin(ax);
-[row col]=size(bi_in);
-p=1;
- for(i=1:1:row)
-     for(j=1:1:col)
-         re_bi_in(p)=str2num(bi_in(i,j));
-         p=p+1;
-     end
+plot(tt,Tx_sig,'r','linewidth',3), grid on;
+title('QPSK modulated signal (sum of inphase and Quadrature phase signal)');
+xlabel('time(sec)');
+ylabel(' amplitude(volt0');
+% QPSK demodulation
+Rx_data=[];
+Rx_sig=Tx_sig; % Received signal
+for(i=1:1:length(data)/2)
+ %%XXXXXX inphase coherent dector XXXXXXX
+ Z_in=Rx_sig((i-1)*length(t)+1:i*length(t)).*cos(2*pi*f*t);
+ % above line indicat multiplication of received & inphase carred signal
+
+ Z_in_intg=(trapz(t,Z_in))*(2/T);% integration using trapizodial rull
+ if(Z_in_intg>0) % Decession Maker
+ Rx_in_data=1;
+ else
+ Rx_in_data=0;
  end
- 
-disp('re-obtain binary information after M-ary demodulation');
-disp(re_bi_in')
-fprintf('\n');
- 
-x=re_bi_in;
-bp=.000001;                                                    
-bit=[]; 
-for n=1:1:length(x)
-    if x(n)==1
-       se=ones(1,100);
-    elseif x(n)==0
-        se=zeros(1,100);
-    end
-     bit=[bit se];
+
+ %%XXXXXX Quadrature coherent dector XXXXXX
+ Z_qd=Rx_sig((i-1)*length(t)+1:i*length(t)).*sin(2*pi*f*t);
+ %above line indicat multiplication ofreceived & Quadphase carred signal
+
+ Z_qd_intg=(trapz(t,Z_qd))*(2/T);%integration using trapizodial rull
+ if (Z_qd_intg>0)% Decession Maker
+ Rx_qd_data=1;
+ else
+ Rx_qd_data=0;
+ end
+
+
+ Rx_data=[Rx_data Rx_in_data Rx_qd_data]; % Received Data vector
 end
- 
-t1=bp/100:bp/100:100*length(x)*(bp/100);
- 
-subplot(3,1,3);
-plot(t1,bit,'lineWidth',2.5);grid on;
-axis([ 0 bp*length(x) -.5 1.5]);
-ylabel('Amplitude(volt)');
-xlabel('Time(sec)');
-title('Receiving information as digital signal after M-ary demoduation');
+figure(3)
+stem(Rx_data,'linewidth',3)
+title('Information after Receiveing ');
+axis([ 0 11 0 1.5]),
+grid on;
